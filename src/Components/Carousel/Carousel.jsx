0bd3c9a7cfc2b5265aa4a0f9ea7dfd42415364e1
Salahadin <sazluzkhan@gmail.com>
2025-09-16 14:59:55 +0300

    Developed Carousel

---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import classes from "./Carousel.module.css";
+import { Carousel } from "react-responsive-carousel";
+import { img } from "./img/data";
+
+const CarouselComponent = () => {
+  return (
+    <>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showThumbs={false}
+        showStatus={false}
+        swipeable={false}
+      >
+        {img?.map((imageItem) => {
+          return <img src={imageItem} />;
+        })}
+      </Carousel>
+      <div className={classes.hero_img}></div>
+    </>
+  );
+};
+
+export default CarouselComponent;
