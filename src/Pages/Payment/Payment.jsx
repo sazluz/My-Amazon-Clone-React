1b25acdb3122a2754889ff8dc416264c268d2b7e
Salahadin <sazluzkhan@gmail.com>
2025-09-25 17:19:04 +0300

    Developed Payment Page UI

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,11 +1,71 @@
 import React from "react";
 import LayOut from "../../Components/LayOut/LayOut";
 import classes from "./Payment.module.css";
+import { DataContext } from "../../Components/DataProvider/DataProvider";
+import { useContext } from "react";
+import ProductCard from "../../Components/Product/ProductCard";
+import { CardElement } from "@stripe/react-stripe-js";
+import CurrencyFormat from "../../Components/CurrencyFormat/CurrencyFormat";
 
 const Payment = () => {
+  const [{ basket, user }] = useContext(DataContext);
+  const total = basket.reduce(
+    (amount, item) => item.price * item.amount + amount,
+    0
+  );
+
   return (
     <>
-      <LayOut>Payment</LayOut>
+      <LayOut>
+        {/* Header */}
+        <div className={classes.payment_header}>
+          Checkout ({basket.length}) items
+        </div>
+        {/* Payment Method */}
+        <section className={classes.payment}>
+          {/* Address */}
+          <div className={classes.flex}>
+            <h3>Delivery Adress</h3>
+            <div>
+              <div>{user?.email}</div>
+              <div>123 Location</div>
+              <div>Chicago</div>
+            </div>
+          </div>
+          <hr />
+          {/* Product */}
+          <div className={classes.flex}>
+            <h3>Review items and delivery</h3>
+            <div>
+              <div>
+                {basket?.map((item) => (
+                  <ProductCard key={item.id} product={item} flex={true} />
+                ))}
+              </div>
+            </div>
+          </div>
+          <hr />
+          {/* Card Form */}
+          <div className={classes.flex}>
+            <h3>Payment Methods</h3>
+            <div className={classes.payment_card_container}>
+              <div className={classes.payment__detail}>
+                <form action="">
+                  <CardElement />
+                  <div className={classes.payment__price}>
+                    <div>
+                      <span>
+                        Total Price | <CurrencyFormat amount={total} />
+                      </span>
+                    </div>
+                    <button>Pay Now</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+      </LayOut>
     </>
   );
 };
--- src/Router.jsx
+++ src/Router.jsx
@@ -7,6 +7,12 @@ import Orders from "./Pages/Orders/Orders";
 import Cart from "./Pages/Cart/Cart";
 import Results from "./Pages/Results/Results";
 import ProductDetail from "./Pages/ProductDetail/ProductDetail";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51SAXzG52d2ibbFcKcIPP5XM6bqESrqPXEqHdJwBSbtU4KdgswwwCLQaATrfaugyrdZNEgrvkmNzq7PPbqUJC0GWW00WiNySA3q"
+);
 
 const Routering = () => {
   return (
@@ -14,7 +20,14 @@ const Routering = () => {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/payments" element={<Payment />} />
+        <Route
+          path="/payments"
+          element={
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+          }
+        />
         <Route path="/orders" element={<Orders />} />
         <Route path="/category/:categoryName" element={<Results />} />
         <Route path="/products/:productId" element={<ProductDetail />} />
